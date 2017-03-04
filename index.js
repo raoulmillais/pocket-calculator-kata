510e6ef693664ac15e5ba5883beb7ee2ab9d93ad
JGAntunes <j.goncalo.antunes@gmail.com>
2017-03-04 11:04:54 +0000

    chore: basic project structure and calculator class

---
+++ index.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('0. ', (answer) => {
+  // console.log(`Thank you for your valuable feedback: ${answer}`);
+  setTimeout(() => rl.close(), 2000)
+
+
+});
